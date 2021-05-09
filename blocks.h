4977c3691094e3ba007644bcfeef91ed74a4ade6
louay <ayyl0uu@pm.me>
2021-05-09 10:52:43 +0100

    change update interval of packages module to 0

--- blocks.h
+++ blocks.h
@@ -1,11 +1,11 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-	{" ",  "$HOME/.scripts/bar/bar-packages",3600,  22},
-	{"",  "$HOME/.scripts/bar/bar-volume", 	 0,    10},
+	{" ","$HOME/.scripts/bar/bar-packages",  0,    22},
+	{"", "$HOME/.scripts/bar/bar-volume", 	 0,    10},
 	{"", "$HOME/.scripts/bar/bar-mem", 	 5,     1},
 	{"", "$HOME/.scripts/bar/bar-temp", 	15,     1},
-	{"",   "$HOME/.scripts/bar/bar-date", 	60,     1},
+	{"", "$HOME/.scripts/bar/bar-date", 	60,     1},
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
